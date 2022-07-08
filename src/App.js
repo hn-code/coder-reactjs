9a03111a49bd0d9a031c8e19e87ec3ee765ae042
Hernan Nievas <hernan.nievas.18@gmail.com>
2022-07-08 17:59:27 -0300

    primer commit de react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Ah Vue, ya está, React es la piedra Angular de nuestro futuro como webdevelopers (?
         </p>
         <a
           className="App-link"
